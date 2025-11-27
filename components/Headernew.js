12bb42383b85468a1bba9f79171e0044263b0bd4
muneeswaran <muneeswaran@eywamedia.com>
2025-11-27 16:42:57 +0530

    fix add search suggestion in header

--- components/Headernew.js
+++ components/Headernew.js
@@ -1231,7 +1231,9 @@ useEffect(() => {
             className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer"
           >
             <img
-              src={item.image || "/no-image.png"}
+              src= {
+                           `/uploads/products/${item.images?.[0]}`
+                          }
               alt={item.name}
               className="w-10 h-10 object-contain rounded"
             />
