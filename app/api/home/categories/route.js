d0b05f5a2f70d8a5ead996a7086d775b8a800aea
muneeswaran <muneeswaran@eywamedia.com>
2026-01-31 13:47:25 +0530

    sm new home page design update7

--- app/api/home/categories/route.js
+++ app/api/home/categories/route.js
@@ -5,10 +5,25 @@ import Category from "@/models/ecom_category_info";
 export async function GET() {
   await dbConnect();
 
+  // Your preferred order
+  const order = [
+    "mobiles",
+    "smart-tv",
+    "tablets",
+    "air-conditioner",
+    "laptop-desktops",
+    "accessories"
+  ];
+
   const categories = await Category.find({ parentid: "none" })
     .select("category_name category_slug image")
-    .limit(7)
     .lean();
 
-  return NextResponse.json(categories);
+  // Custom sort
+  const sortedCategories = categories.sort(
+    (a, b) =>
+      order.indexOf(a.category_slug) - order.indexOf(b.category_slug)
+  );
+
+  return NextResponse.json(sortedCategories);
 }
